d0a77ef0b4ee71a9aff37d13c11d4a37f5a12577
Ritushwar <ritushwarneupane111@gmail.com>
2026-07-01 19:20:39 +0545

    parameter is added

--- src/custom_msg_pub_sub/custom_msg_pub_sub/publisher.py
+++ src/custom_msg_pub_sub/custom_msg_pub_sub/publisher.py
@@ -13,24 +13,35 @@ topic = "robot_status"
 class PublishStatus(Node):
     def __init__(self):
         super().__init__('robotStatusPublisher')
+
+        # Declare the parameter
+        self.declare_parameter('robot_name', "Sophus")
+        self.declare_parameter('publish_period', 1.0)
+        self.declare_parameter('initial_battery', 100.0)
+        self.declare_parameter('drain_rate', 0.5)
+
+        # Read the new parameter value
+        self.robot_name = self.get_parameter('robot_name').value
+        self.period = self.get_parameter('publish_period').value
+        self.charge = self.get_parameter('initial_battery').value
+        self.drain_rate = self.get_parameter('drain_rate').value
+
         self.publisher = self.create_publisher(
             RobotStatus,
             topic,
             5
         )
 
-        # period ofr publishing the message
-        self.period = 1
         self.timer = self.create_timer(self.period, self.my_callback_fn)
         self.counter = 0
-        self.charge = 100
 
     def my_callback_fn(self):
         my_status = RobotStatus()
-        self.charge = self.charge - 0.05
+        self.charge = self.charge - self.drain_rate
         if self.charge < 0:
-            self.charge = 100
-        my_status.robot_name = "Sophus"
+            self.charge = self.get_parameter("initial_battery").value
+
+        my_status.robot_name = self.robot_name
         my_status.battery_level = self.charge
         my_status.is_charging = False
         my_status.error_code = 404
