2f8c93c54324e848241ed7864c04d1ebbec6fa5d
Ritushwar <ritushwarneupane111@gmail.com>
2026-07-06 22:18:11 +0545

    launch file for my_robot

---
+++ src/my_robot_description/launch/my_robot.launch.py
@@ -0,0 +1,30 @@
+import os
+from ament_index_python.packages import get_package_share_directory  # finds where a ROS 2 package is installed
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg_path = get_package_share_directory('my_robot_description')
+    urdf_file = os.path.join(pkg_path, 'urdf', 'my_robot.urdf')
+
+    with open(urdf_file, 'r') as f:
+        robot_description = f.read()
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{'robot_description': robot_description}]
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2'
+        ),
+     ]
+     )
